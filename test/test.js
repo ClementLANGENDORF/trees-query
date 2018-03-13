b85f32b7d9c0aa2fb32067b919887d3803ec06ff
slim <smedini@gmail.com>
2018-03-13 18:39:43 +0100

    v1

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import {object, withId} from "../src";
+import mongo from 'mongodb';
 
 chai.should();
 
@@ -9,7 +10,11 @@ beforeEach(async () => {
 
 const id = "5a6a03c03e77667641d2d2c3";
 
-describe('TU Init', function () {
+describe('TU query', function () {
+
+    it('object', async function () {
+        object(id).should.deep.equal(new mongo.ObjectID(id));
+    });
 
     it('withId', async function () {
         withId(id).should.deep.equal({_id: object(id)});
